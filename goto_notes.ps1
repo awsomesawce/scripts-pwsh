1491290f04ac474025009caf80d0f897077011d7
Carl C <awsomesawce@outlook.com>
2020-10-25 19:13:16 -0400

    edit goto_notes.ps1

--- goto_notes.ps1
+++ goto_notes.ps1
@@ -1,5 +1,5 @@
 # This is a quick script that will allow me to go to my notes folder in powershell
-set-location "D:\Carl\Onedrive\Notable\Notes"
+set-location -path "D:\Carl\Onedrive\Notable\Notes" -PassThru
 date >> from_code.md
-echo >> "Here's a new note made from the terminal" >> from_code.md
+echo "Here's a new note made from the terminal" >> from_code.md
 code from_code.md
