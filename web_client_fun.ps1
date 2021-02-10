5a8d353ce29a5220c9c321ae69e7f9b22721c888
Carl C <awsomesawce@outlook.com>
2021-02-10 15:29:03 -0500

    add web_client_fun.ps1

---
+++ web_client_fun.ps1
@@ -0,0 +1,7 @@
+# Taken from Microsoft website
+
+$wc = [System.Net.WebClient]::new()
+$pkgurl = 'https://github.com/PowerShell/PowerShell/releases/download/v6.2.4/powershell_6.2.4-1.debian.9_amd64.deb'
+$publishedHash = '8E28E54D601F0751922DE24632C1E716B4684876255CF82304A9B19E89A9CCAC'
+$FileHash = Get-FileHash -InputStream ($wc.OpenRead($pkgurl))
+$FileHash.Hash -eq $publishedHash
