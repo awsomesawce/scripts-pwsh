471b824558125914d4d3cd505d7a9f9e81bdcdba
Carl C <awsomesawce@outlook.com>
2021-02-24 14:28:27 -0500

    Add project variable script

---
+++ config/projectvars.ps1
@@ -0,0 +1,18 @@
+# A list of variables that point to project directories
+# TODO: add second script for sourcing functions
+# Author: Carl C (awsomesawce@outlook.com)
+
+set-variable -Name labscurrent -Value "~/Downloads/labs-node/labs-jan-5-2021/labs" -Description "Current labs"
+
+Set-Variable -Name nodewebpackproj -Value "D:\Carl\Documents\GitHub\my-webpack-demo" -Description "My webpack demo git dir"
+
+Set-Variable -Name pwshsnippets -Value "D:\Carl\OneDrive\snippets\pwsh\powershell_snippets.txt" -Description "out-file for writing quick powershell snippets from the command line"
+
+Set-Variable -Name NVIMINITVIM -Value C:\Users\Carl\AppData\Local\nvim\init.vim -Description "Main config file for neovim"
+Set-Variable -Name DESKTOP -Value D:\Carl\OneDrive\Desktop\ -Description "Shortcut to the Desktop folder"
+Set-Variable -Name randomoutput -Value D:\Carl\OneDrive\TODO\randomoutput.md -Description "A place to pipe output from pwsh"
+Set-Variable -Name GitDirectoryD -Value D:\Carl\Documents\GitHub -Description "Main directory for github hosted projects"
+
+$env:EDITOR = "nvim" # EDITOR variable to use when invoking unix-like programs
+
+Write-Output "Done Processing variables"
