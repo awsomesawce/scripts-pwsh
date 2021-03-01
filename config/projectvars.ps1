57d44b2c541912d0b860cb4f726f405c5fb4a32d
Carl C <awsomesawce@outlook.com>
2021-02-28 20:49:44 -0500

    tidy up projectvars

--- config/projectvars.ps1
+++ config/projectvars.ps1
@@ -9,10 +9,12 @@ Set-Variable -Name nodewebpackproj -Value "D:\Carl\Documents\GitHub\my-webpack-d
 Set-Variable -Name pwshsnippets -Value "D:\Carl\OneDrive\snippets\pwsh\powershell_snippets.txt" -Description "out-file for writing quick powershell snippets from the command line"
 
 Set-Variable -Name NVIMINITVIM -Value C:\Users\Carl\AppData\Local\nvim\init.vim -Description "Main config file for neovim"
-Set-Variable -Name DESKTOP -Value D:\Carl\OneDrive\Desktop\ -Description "Shortcut to the Desktop folder"
+Set-Variable -Name DESKTOP -Value D:\Carl\OneDrive\Desktop\ -Description "Shortcut to the Desktop folder"
 Set-Variable -Name randomoutput -Value D:\Carl\OneDrive\TODO\randomoutput.md -Description "A place to pipe output from pwsh"
 Set-Variable -Name GitDirectoryD -Value D:\Carl\Documents\GitHub -Description "Main directory for github hosted projects"
 
-$env:EDITOR = "nvim" # EDITOR variable to use when invoking unix-like programs
-
-Write-Output "Variables have been imported from $PSScriptRoot"
+$env:EDITOR = "nvim" # EDITOR variable to use when invoking unix-like programs.
+$env:PAGER = "less" # PAGER variable used by a ton of unix-like programs.
+Write-Output "Set `$env:PAGER to $env:PAGER"
+Write-Output "Set `$env:EDITOR to $env:EDITOR"
+Write-Output "Variables have been imported from $PSScriptRoot\projectvars.ps1"
