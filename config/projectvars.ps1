3f2e1e9e73d3e5e2266b8660bdc77a350886aaf3
Carl C <awsomesawce@outlook.com>
2021-03-11 10:39:47 -0500

    Update projectvars.ps1

--- config/projectvars.ps1
+++ config/projectvars.ps1
@@ -3,6 +3,10 @@
 # TODO: add second script for sourcing functions
 # Author: Carl C (awsomesawce@outlook.com)
 
+# First, set the variable to this script.
+Set-Variable -Name projectVarsScript -Value "C:\Users\Carl\gitstuff\scripts-pwsh\config\projectvars.ps1"
+
+# Then, set the rest of the current project variables.
 set-variable -Name labscurrent -Value "~/Downloads/labs-node/labs-jan-5-2021/labs" -Description "Current labs"
 
 Set-Variable -Name nodeschool -Value "D:\Carl\Documents\GitHub\node-school\" -Description "node-school directory"
@@ -16,7 +20,7 @@ Set-Variable -Name DESKTOP -Value D:\Carl\OneDrive\Desktop\ -Description "Shortc
 Set-Variable -Name randomoutput -Value D:\Carl\OneDrive\TODO\randomoutput.md -Description "A place to pipe output from pwsh"
 Set-Variable -Name GitDirectoryD -Value D:\Carl\Documents\GitHub -Description "Main directory for github hosted projects"
 
-function nodesch {set-location GitDirectoryD\node-school}
+function nodesch {set-location $nodeschool}
 
 $env:EDITOR = "nvim" # EDITOR variable to use when invoking unix-like programs.
 $env:PAGER = "less" # PAGER variable used by a ton of unix-like programs.
