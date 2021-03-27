a2cb128167e487a11b7d1fb158303b1dc063720e
Carl C <awsomesawce@outlook.com>
2021-03-27 14:19:08 -0400

    Made changes to profile, changes to sourced scripts.

--- config/Microsoft.PowerShell_profile.ps1
+++ config/Microsoft.PowerShell_profile.ps1
@@ -5,20 +5,23 @@ Import-Module posh-git
 Import-Module oh-my-posh
 #Import-Module z
 # Set prompt
-# Source variables for current projects into PSSession
+Set-PoshPrompt -Theme zash && Write-Information "Set prompt to zash"
+# Source other_functions script and projectvars script.
 $PSDirectory="D:/Carl/Documents/Powershell"
+$Script:scriptspwsh = "$env:USERPROFILE\gitstuff\scripts-pwsh\config"
+# Try ternary operation.
+$otherFunctionsScript = "$Script:scriptspwsh/other_functions.ps1"
 
-$otherFunctionsScript = "$PSDirectory/Scripts/other_functions.ps1"
-if (Test-Path "$otherFunctionsScript") {
-    . "$otherFunctionsScript"
-    Write-Progress "`$otherFunctionsScript = $otherFunctionsScript and 
-    has been loaded."
-} else {
-    "$otherFunctionsScript file not found."
-}
+(Test-Path $otherFunctionsScript) ? (. $otherFunctionsScript) : (Write-Output "`$otherFunctionsScript not found here: $otherFunctionsScript")
 
-$scriptspwsh = "$env:USERPROFILE\gitstuff\scripts-pwsh\config"
-$projectvarsScript = "$scriptspwsh\projectvars.ps1"
+# if (Test-Path "$otherFunctionsScript") {
+#     . "$otherFunctionsScript"
+#     Write-Output "`$otherFunctionsScript has been loaded.  Its path = $otherFunctionsScript"
+# } else {
+#     "$otherFunctionsScript file not found."
+# }
+
+$projectvarsScript = "$Script:scriptspwsh\projectvars.ps1"
 (Test-Path $projectvarsScript) ? (. $projectvarsScript) : (Write-Output "projectvars.ps1 not found")
 # If ternary operator is incompatible, use this:
 # if (Test-Path ~/gitstuff/scripts-pwsh/config/projectvars.ps1) {
@@ -28,7 +31,7 @@ $projectvarsScript = "$scriptspwsh\projectvars.ps1"
 # }
 # set psdir variable to local powershell directory, and set other variables.
 $gitDir="D:/Carl/Documents/GitHub"
-$oneDrive="D:/Carl/OneDrive"
+$oneDrive="$env:OneDrive"
 $localAppData="C:/Users/Carl/AppData/Local"
 $globalAppData="D:/Carl/Appdata"
 # The environment variable below sets XDG_CONFIG_HOME, which then tells nvim to look for it's
@@ -40,44 +43,37 @@ $globalAppData="D:/Carl/Appdata"
 ## This option sets the command line editor to have Emacs-like keybindings.
 ## You can set more options using Set-PSReadlineKeyHandler
 #Set-PSReadLineOption -EditMode Windows
-$DOTFILESGIT = "$HOME/gitstuff/my-dotfiles"
+$DOTFILESGIT = "$env:USERPROFILE\gitstuff\my-dotfiles"
 function dotFiles {Set-Location $DOTFILESGIT}
 # Aliases
 # use single-quotes for strings with spaces
 # TODO: In progress: Moving Set-Alias declarations to separate
-# file.
+# file. other_functions.ps1 in config dir.
 Function lsWide {Get-ChildItem | Format-Wide}
 Set-Alias -Name lsw -Value lsWide -Description 'ls but format-wide'
 Set-Alias -Name list -Value Get-ChildItemColorFormatWide
+Set-Alias -Name lsc -Value Get-ChildItemColorFormatWide
 
 ## Set-Location to a wsl distro file system
 Function gotoubuntu {Set-Location \\wsl$\Ubuntu-20.04\home\carlc}
 
 ## Open Powershell profile from anywhere
 ## Just type $EDITOR $PROFILE where $EDITOR is any text editor.
-## Nvim config shortcut # remember to use backslashes.
-$nvimInitFile = "$localAppData\nvim\init.vim"
-function gotonvimconfig {set-location $localAppData\nvim}
-Function nvimconfig {nvim $localAppData\nvim\init.vim}
-Function neovconfig {neovide.ps1 $localAppData\nvim\init.vim}
+# IMPORTANT: Copied nvim config functions to other_functions.ps1
 ## Nvim 2 window shortcut
 Function nvim2 {nvim -O2}
 Function nvim22 {nvim -O2 -p2}
-Function fvim22 {fvim -O2 -p2}
 Function nvimdiff {nvim -d}
-Function fvimdiff {fvim -d}
 function nvimqtdiff {nvim-qt -d $args}
 ## Shortcut for duckduckgo
 $duckduck = "https://duckduckgo.com"
-function ducks { Start-Process $duckduck }
-$CYGDIR = (Get-Item "D:\Cygwin")
-Set-Variable -Name CYGBIN -Value "$CYGDIR\bin" -Description "Variable that points to Cygwin's bin directory."
-Set-Alias -Name np -Value notepad.exe -Description "A shorter notepad alias."
+function ducks { Start-Process $duckduck/?q=$args }
 # This one sets an alias for the ri command in ruby
 set-alias -name rubyri -value D:\Ruby27-x64\bin\ri.cmd -Description "A workaround for ruby's ri, cuz in pwsh ri is remove-item"
-Set-Alias -Name exp -Value explorer
+Set-Alias -Name exp -Value explorer.exe
 Set-Alias -Name man -Value D:\Cygwin\bin\man.exe -Description "Show info file from cygwin which includes a lot more documentation than the windows emacs info"
-Set-Alias -Name lsc -Value Get-ChildItemColorFormatWide -Description "Better alias for color ls"
+#Set-Alias -Name lsc -Value Get-ChildItemColorFormatWide -Description "Better alias for color ls"
+# The above alias is already in your other_functions.ps1
 function linuxls {D:\Cygwin\ls.exe --color=always --group-directories-first}
 Set-Alias -Name lxls -Value linuxls -Description "Linux type of ls."
 Set-Alias -Name rdis -Value rdiscount -Description "Shorter rdiscount"
@@ -87,8 +83,10 @@ Set-Alias -Name msys -Value D:\MSYS2\msys2_shell.cmd -Description "Use --help fo
 Set-Alias -Name git-cola -Value C:\Users\Carl\AppData\Local\git-cola\bin\git-cola.exe -Description "Git-cola because "
 ## Beware when starting shells directly from cygwin's bin.
 ## It will not inherit the necessary env variables from Windows.
-Set-Alias -Name cygbash -Value D:\Cygwin\bin\bash.exe -Description "Use man bash for help"
 Set-Alias -Name cygtcsh -Value D:\Cygwin\bin\tcsh.exe -Description "Cygwin's tcsh"
+$CYGDIR = (Get-Item "D:\Cygwin")
+Set-Variable -Name CYGBIN -Value "$CYGDIR\bin" -Description "Variable that points to Cygwin's bin directory."
+Set-Alias -Name cygbash -Value "$CYGBIN\bash.exe"
 ## TODO: Move Aliases and Functions into their own respective files and source each file.
 ## Functions
 
@@ -108,7 +106,6 @@ Function nodeschool {Set-Location -Path "D:\Carl\Documents\GitHub\node-school"}
 # get to emacs org directory located in OneDrive quickly.
 Function org_dir {Set-Location -Path $oneDrive\org_dir}
 # This is a function for git status, not Get-GitStatus, which is a posh-git cmdlet.
-Function gitst {git status}
 Function gpgmee {gpg -se -r Carl}
 Function localAppData {Set-Location -Path C:\Users\Carl\AppData\Local}
 Function chdirGAppData {Set-Location -Path $globalAppData}
@@ -120,18 +117,16 @@ Function gotodotfilesbackup {Set-Location -Path $oneDrive\dotfiles_backup}
 Function lspage {Get-ChildItem | less}
 Set-Alias -Name lsl -Value lspage
 Set-Alias -Name dotdir -Value gotodotfilesbackup
-Set-Alias -Name lg -Value 'lazygit'
-Set-Alias -name localad -Value localappdata
+Set-Alias -Name lg -Value 'lazygit' -Description "lazygit is a git management cli"
+Set-Alias -name localad -Value localappdata 
 Set-Alias -Name gitbash -Value 'D:\Program Files\Git\git-bash.exe'
-Set-Alias -Name ... -Value D:\Carl\Documents\PowerShell\Scripts\backwards_cd.ps1
-Set-Alias -Name .. -Value D:\Carl\Documents\PowerShell\Scripts\backwards_cd1.ps1
+# TODO: Instead of creating external scripts, you can create a function for each.
+Set-Alias -Name ... -Value D:\Carl\Documents\PowerShell\Scripts\backwards_cd.ps1 -Description "go up two directories."
+Set-Alias -Name .. -Value D:\Carl\Documents\PowerShell\Scripts\backwards_cd1.ps1 -Description "go up a directory"
 Set-Alias -name ipinfo -Value D:\Carl\Documents\PowerShell\Scripts\getipinfo.ps1
 Set-alias -Name invo -Value Invoke-Item -description "Shorter invoke-item"
 # Alias for Get-GitStatus function provided by posh-git
 set-alias -Name gitstat -Value Get-GitStatus
-Set-Alias -Name list -Value Get-ChildItemColor
-# Above makes the ls command always use Get-ChildItemColor
-# This attaches the rename command to rename item
 # ren is also an alias for rename-item
 Set-Alias -Name rename -Value Rename-Item -Description "A smart rename alias"
 Set-Variable -Name CYGBIN -Value 'D:/Cygwin/bin' -Description 'Location for cygwin binaries'
--- config/other_functions.ps1
+++ config/other_functions.ps1
@@ -1,27 +1,29 @@
 ##### Other PS Functions #####
 # Please add functions to this file instead of the main powershell profile.
+# Author: Carl C. (awsomesawce@github.com)
 
 ## Aliases for common Cmdlets
 # These aliases aren't defined by default, but they are quite useful.
 #
-Set-Alias cfjs -Value ConvertFrom-Json
+Set-Alias cfjs -Value ConvertFrom-Json -Description "Convertfrom json in 4 letters"
+Set-Alias c2json -Value Convertto-Json -Description "Different from ConvertFrom-Json by 2 letters"
 Set-Alias add -Value Add-Content -Description "An alias for Add-Content that just makes sense."
 Set-Alias -Name "sob" -Value "Select-Object" -Description "A shorter select"
-Set-Alias -Name rename -Value Rename-Item -Description "A smart rename alias"
 Set-Alias ghlp -Value Get-Help -Description "A short get-help"
-
+Set-Alias -Name "hlp" -Value "Help" -Description "An even shorter help.  One less letter."
+#
 # TODO: Create a function for viewing Get-Help output in a pager.
-
-function getHelpPage {
-    Get-Help "$@" | less
-}
-
-
-## Aliases for common external commands and programs, like Git
+# UPDATE: Function isn't needed.  Powershell 7 detects the
+# $env:PAGER variable which was set in the profile.  If it has `less` as the PAGER,
+# then using `Help` will then use `less` as it's pager program.
+# This will allow you to go up or down when viewing the documentation.
+#
+# Aliases for common external commands and programs, like Git
 # INFO: In powershell, Aliases can generally have no arguments.  If you want to add arguments to an alias
 # you need to write a function first.
 #
 Set-Alias -Name g -Value git -Description "Git in one letter"
+Function gitst {git status}
 Set-Alias -Name l -Value Get-ChildItem -Description "List items in directory in one letter"
 Set-Alias -Name np -Value notepad.exe -Description "A simple way to open notepad"
 Function Open-Node-Docs {Start-Process https://nodejs.org/dist/latest-v14.x/docs/api/}
@@ -37,16 +39,21 @@ Function wslubuntu {
 }
 Set-Alias -Name wslubu -Value wslubuntu -Description "Shorter wsl ubuntu command"
 
+# Next up is editor functions!
+# Neovim config shortcut # remember to use backslashes.
+$nvimInitFile = "$localAppData\nvim\init.vim"
+function gotonvimconfig {set-location $localAppData\nvim}
+Function nvimconfig {nvim $nvimInitFile}
+
 # TODO: test this function for starting a powershell process as Admin.
+# EXPERIMENTAL: Function below is experimental.
 function Start-PSAdmin {Start-Process pwsh -Verb RunAs}
 set-alias -Name split -Value Split-Path
 # Use curl chtsh as a function
 function chtsh { curl cht.sh/$args }
 
 
-# This function can be used if sourced by the powershell session.
-# use `. name-of-script.ps1' and the function is then available to use
-# This script makes the Send-Greeting cmdlet available to the host!
+# This function serves as an example on how to write advanced functions.
 
 function Send-Greeting
 {
@@ -75,3 +82,38 @@ function duks {Start-Process https://duckduckgo.com/?q=$args}
 function kaku {wsl -u carlc kak}
 function gitpushsync {git add . && git commit -m "$args" && git pull && git push }
 function gitpp {git pull && git push}
+
+# The following block is copied from barebones_Functions.ps1
+# barebones_Functions.ps1
+# Author: Carl C (awsomesawce@outlook.com)
+# These are functions that are very helpful but don't require a lot of setup.
+# This is normally sourced by `barebones_pwsh_profile.ps1` and not by itself.
+#
+# psWhich is a function that mimicks the output of the `which` unix command.
+function psWhich {Get-Command $args | Select-Object -ExpandProperty Source}
+# duks allows the user to search from the command line.
+function duks {Start-Process https://duckduckgo.com/?q=$args}
+# gotoexelocation is a nice command that allows you to chdir right to the directory where 
+# the executable is located.  Pretty neat, huh?
+function gotoexelocation {
+    Set-Location (Split-Path -Parent (get-command $args | Select-Object -ExpandProperty Source))
+}
+Set-Alias -Name ExeLocation -Value gotoexelocation -Description "Shorter gotoexelocation"
+function ndir { set-location $env:OneDrive\Notable\notes }
+<#
+# The following command is EXPERIMENTAL
+#
+#>
+function getcommandinfo {
+    # This is a test to see how splatting works.
+    # The $selProps var contains properties that the command should show.
+    $Local:selProps = @("name", "commandtype", "source")
+    Get-Command -Name @args | Select-Object -Property $Local:selProps
+    Write-Information "This is a test"
+ }
+# End-of-block from barebones_Functions.ps1
+
+
+# Let the user know that this file was sourced
+Write-Host "other_functions file has been loaded from here: "
+Write-Host "$otherFunctionsScript"
