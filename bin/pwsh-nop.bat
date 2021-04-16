7add95210b11008fb2c0eedf674610c96e8afd75
Carl C <awsomesawce@outlook.com>
2021-04-16 15:50:26 -0400

    Rename and edit file

--- bin/pwsh-nop.bat
+++ bin/pwsh-nop.bat
@@ -9,17 +9,17 @@ set PAGER=less
 set EDITOR=notepad
 @rem wt.exe new-tab cmd.exe 
 @start wt new-tab pwsh -noprofile
-:: Testfold {{{ ::
-:: }}} Testfold ::
-@rem "cmd.exe commands can be all caps or not"
+:: Explanation {{{ ::
+@rem cmd.exe commands are case-insensitive, unlike Linux/Unix commands.
 @rem To differentiate windows commands from similar linux commands, 
 @rem it is often useful to use the all caps ECHO in batch scripts,
 @rem since ECHO is not a valid way to call linux's echo and won't work.
 @rem START https://duckduckgo.com
 @rem https://github.com
-@rem ----------------------------------------------
-@rem     Welcome to Windows Powershell!
-@rem         Please enjoy your stay!
-@rem      PAGER is set to %PAGER%
-@rem      EDITOR is set to %EDITOR%
-@rem ----------------------------------------------
+:: }}} Explanation ::
+@ECHO ----------------------------------------------
+@ECHO     Welcome to Windows Powershell!
+@ECHO         Please enjoy your stay!
+@ECHO      PAGER is set to %PAGER%
+@ECHO      EDITOR is set to %EDITOR%
+@ECHO ----------------------------------------------
