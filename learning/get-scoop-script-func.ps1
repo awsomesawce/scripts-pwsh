f985e7cec8b626aa4e93f9fb5cf7d23714c3cf06
Carl C <awsomesawce@outlook.com>
2021-04-26 20:27:01 -0400

    Added super cool function to get-scoop-script-func.ps1

--- learning/get-scoop-script-func.ps1
+++ learning/get-scoop-script-func.ps1
@@ -3,17 +3,50 @@
 # installed in the ~/scoop/apps/ subdirectory and then prints out the script
 # to the console!  Cool, huh?
 
+function testthisfunc {
+    <#
+        .Description
+        testthisfunc: Go to some other directory and get a message printed out!"
+        .PARAMETER Path
+        The name of the dir you want to change to.
+        #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true,
+            HelpMessage = "Enter what directory you would like to change to")]
+        [Alias("chtodir", "testfuncyes")]
+        [string] $Path
+    )
+    
+    begin {
+        Write-Output "Going to $Path"
+    }
+    
+    process {
+       Set-Location $Path && Write-Output "That seemed to work!" || Write-Error "That place doesnt exist"
+    }
+    
+    end {
+        Write-Information "This is the end code block"
+    }
+}
+
 function get-scoop-script {
-if ($args) {
-if (test-path -Path "~/scoop/apps/$args") {
-get-content -Raw $(Get-Command -CommandType ExternalScript "$args.ps1").Source
-} else {
-write-error "Application not located in scoop directory"
-}
-} else {
-    Write-Error "You did not write an argument"
-    Write-Error "Usage: get-scoop-script `"name-of-script`""
-}
+    if ($args) {
+        if (test-path -Path "~/scoop/apps/$args") {
+            Write-Output "Found ~/scoop/apps/$args"
+            Write-Output "Getting file contents of $(get-command -CommandType ExternalScript "$args.ps1")"
+            get-content -Raw $(Get-Command -CommandType ExternalScript "$args.ps1").Source
+        }
+        else {
+            write-error "Application not located in scoop directory"
+            Write-Error "$MyInvocation.MyCommand.Definition"
+        }
+    }
+    else {
+        Write-Error "You did not write an argument"
+        Write-Error "Usage: get-scoop-script `"name-of-script`""
+    }
 }
 
 function get-scoop-shim {
@@ -21,7 +54,8 @@ function get-scoop-shim {
         if (test-path -Path "~/scoop/shims/$args.ps1") {
             Write-Information "Getting script source for `"$args`""
             Get-Content -Encoding UTF-8 $(Get-Command -Name "$args.ps1" -CommandType ExternalScript).Source
-        } else {
+        }
+        else {
             Write-Error -Message "Script name of $args.ps1 not found in ~/scoop/shims subdirectory"
         }
         else {
