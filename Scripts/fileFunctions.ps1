238a519443f7967e197e1733b159caa814f4014e
Carl C <awsomesawce@outlook.com>
2021-05-21 16:07:03 -0400

    Add fileFunctions script

---
+++ Scripts/fileFunctions.ps1
@@ -0,0 +1,25 @@
+function Count-Files {
+
+<#
+.Description
+Count-Files: Count the files in a directory
+.PARAMETER Path
+The path of the directory to count files in.
+#>
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $true,
+        HelpMessage = "Enter the path")]
+    [string]$Path
+)
+
+Process {
+    if ($Path) {
+        Write-Output $(Get-ChildItem $Path).Count
+    }
+    else {
+        Write-Error "Need to enter path"
+    }
+}
+
+}
