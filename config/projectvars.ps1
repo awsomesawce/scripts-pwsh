33d818059cc601b6cad49859094ef474d4c6d329
Carl C <awsomesawce@outlook.com>
2021-05-26 13:39:27 -0400

    Adjust projectVars.ps1 script, cleanup

--- config/projectvars.ps1
+++ config/projectvars.ps1
@@ -19,10 +19,9 @@ $myDotfiles = $env:myDotfiles
 $Local:scriptspwshDir = "$gitstuffC\scripts-pwsh"
 $script:configDir = "$Local:scriptspwshDir/config"
 
-# Get the location of the git-tracket pwsh profile and put it in a variable.
+# Get the location of the git-tracked pwsh profile and put it in a variable.
 # Just get the string.
 Set-Variable -Name profileGitTracked -Value "$Script:configDir/Microsoft.PowerShell_profile.ps1"
-
 # Then, set the rest of the current project variables.
 set-variable -Name labscurrent -Value "~/Downloads/labs-node/labs-jan-5-2021/labs" -Description "Current labs"
 
@@ -30,20 +29,28 @@ set-variable -Name labscurrent -Value "~/Downloads/labs-node/labs-jan-5-2021/lab
 # NOTE: Set-Variable only refers to the scope of the Powershell session, and doesn't share vars with other processes.
 Set-Item -Path Env:\UserProfileD -Value (Get-Item "D:\Carl") # This is a variable that leads to my D drive user profile.
 
+# TODO: Consolidate some important project vars into arrays or hash tables!  
+#+ Great idea!
+
 Set-Variable -Name GitDirectoryD -Value $UserProfileD\Documents\GitHub -Description "Main directory for github hosted projects"
-
 Set-Variable -Name nodeschool -Value "D:\Carl\Documents\GitHub\node-school\" -Description "node-school directory"
-
 Set-Variable -Name nodewebpackproj -Value "D:\Carl\Documents\GitHub\my-webpack-demo" -Description "My webpack demo git dir"
-
 Set-Variable -Name pwshSnippetsFile -Value "D:\Carl\OneDrive\snippets\pwsh\powershell_snippets.txt" -Description "out-file for writing quick powershell snippets from the command line"
 Set-Variable -Name pwshSnippetsDir -Value (Split-Path -Parent "$pwshSnippetsFile")
-
 Set-Variable -Name NVIMINITVIM -Value C:\Users\Carl\AppData\Local\nvim\init.vim -Description "Main config file for neovim"
 Set-Variable -Name DESKTOP -Value $env:OneDrive\Desktop\ -Description "Shortcut to the Desktop folder"
 Set-Variable -Name randomnotes -Value $env:OneDrive\TODO\randomoutput.md -Description "A place to pipe output from pwsh"
 
-function nodesch {set-location $nodeschool}
+# This is a redo of the nodesch function that was previously here.
+function Goto-NodeSchool {
+    param([switch]$Labs)
+    if (!($Labs)) {
+	# This function only works in Powershell Core (ternary operators)
+	(Test-Path $nodeschool) ? (Set-Location -Path $nodeschool) : (Write-Error -Category ResourceUnavailable -Message "$nodeschool not found")
+    } else {
+	return Write-Error -Category NotImplemented -Message "This part of the script will set location to where school labs are on the filesystem."
+    }
+}
 
 # UPDATE: Added EDITOR and PAGER declarations to main $PROFILE
 #$env:EDITOR = "vim" # EDITOR variable to use when invoking unix-like programs.
@@ -71,27 +78,13 @@ set-alias -Name dotent -Value dotnet -Description "Start dotnet on spelling erro
 set-alias -Name get-hlep -Value get-help -Description "Spelling error correction"
 $snippets = "$env:OneDrive\snippets"
 
-# Mon May  3 20:21:31 EDT 2021 Remove cygwininstaller variables
-
 $barebonesDir = "D:\Carl\OneDrive\snippets\pwsh\barebones\"
 $barebonesFunctionScript = Get-Item "D:\Carl\OneDrive\snippets\pwsh\barebones\barebones_Functions.ps1"
 
-# TODO: Put this function in a _misc_functions.ps1 file or something.
+# TODO: Put this function in a _misc_functions.ps1 file or something. :DONE:
 
-function open-abs-guide {
-$local:abslocation = "C:\Users\Carl\Downloads\info\abs-guide\"
-if (test-path "$abslocation") {
-invoke-item "$abslocation/index.html"
-} else {
-echo "the location of abs-guide is not found"
-}
-return "Opening abs guide"
-}
-# The above function is personal to my computer so I put it in projectVars.ps1 instead of other_functions.ps1
-set-alias absguide -Value open-abs-guide -Description "Advanced bash scripting guide"
-$msysZshExe = "D:\MSYS2\usr\bin\zsh.exe"
 set-variable documents -Value "D:\Carl\Documents" -Description "Documents folder that explorer points to.  Powershell folder is also in this folder"
-$dotfileDirs = (Get-Item D:\Carl\OneDrive\dotfiles_backup\), (Get-Item C:\Users\Carl\gitstuff\my-dotfiles\)
+$dotfileDirs = @((Get-Item D:\Carl\OneDrive\dotfiles_backup\), (Get-Item C:\Users\Carl\gitstuff\my-dotfiles\))
 $zshlovers = get-item D:\Carl\OneDrive\snippets\bash\zsh-only\zsh-lovers-manpage.html
 $curSnipFile = Get-Item D:\Carl\OneDrive\snippets\pwsh\powershell_learning_new.ps1
 $editorconfigtemplate = "C:\Users\Carl\gitstuff\my-dotfiles\templates\template.editorconfig"
@@ -140,6 +133,17 @@ $backupDirs = @{
     windows_dotfiles_backup="$windowsDotfilesBackup"
 
 }
+
+# ImportantHash hash table
+# EXPERIMENTAL: This is for testing nested hash tables
+
+$ImportantHashHash = @{
+    backupDirs = $backupDirs
+    newDocDirs = $newestDocumentDirs
+}
+
+$ImportantHashArr = @($backupDirs, $newestDocumentDirs)
+
 # This is the simplest function for listing powershell notes in my Notable
 #+ directory.
 function listPowershellNotes {
