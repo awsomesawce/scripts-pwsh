56b3d5b2073b928d1b38e5554f0d24f83ffc2dd9
Carl C <awsomesawce@outlook.com>
2021-07-05 05:23:49 -0400

    Update python filesys locations

--- config/pyFileSysLocations.ps1
+++ config/pyFileSysLocations.ps1
@@ -10,4 +10,10 @@ $py39msys = "D:\MSYS2\usr\bin\python3.9.exe"
 $py38 = "C:\Users\Carl\AppData\Local\Microsoft\WindowsApps\python3.8.exe"
 # Var pointing to system-site scripts installed by Windows Store python3.8:
 $py38Scripts = "C:\Program Files\Python38\Scripts"
-
+# Var pointing to user-site scripts installed by Windows Store python3.8:
+$py38ScriptsUser = "D:/Carl/AppData/Python/Python38/Scripts"
+# Var containing list of scripts in user-site scripts dir:
+$py38ScriptsList = (Get-ChildItem $py38ScriptsUser)
+# Var pointing to system-site scripts for Windows Store python3.9 -m pip
+# `--user` and regular install both install to this folder (dir).
+$py39Scripts = "C:\Users\Carl\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.9_qbz5n2kfra8p0\LocalCache\local-packages\Python39\Scripts"
