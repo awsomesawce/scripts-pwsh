d1095f90f3bb85080f50be22251a47ed1ac60656
Carl C <awsomesawce@outlook.com>
2021-08-01 19:24:24 -0400

    Add completion, edit start-condaenv.ps1

--- bin/start-condaenv.ps1
+++ bin/start-condaenv.ps1
@@ -1,3 +1,7 @@
 # Taken from Anaconda start-menu shortcut on Windows.
 # This allows the user to start the conda env from the current powershell session.
-& 'C:\Users\Carl\miniconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\Users\Carl\miniconda3' 
+if ($env:CONDA_EXE) {
+    Write-Error "Conda environment is already loaded"
+} else {
+    & 'C:\Users\Carl\miniconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\Users\Carl\miniconda3' 
+}
