549eccf4c1ed18e76cfca73c9654576243d18140
Carl C <69673640+awsomesawce@users.noreply.github.com>
2021-09-24 08:53:45 -0400

    Config-New: offload profile functions to Begin-Module.psm1

---
+++ config-new/Begin-Module.psm1
@@ -0,0 +1,51 @@
+# Beginner Functions Module
+# Starting module for new powershell setup
+
+function duks {
+
+    <#
+    .Description
+    duks -Search "search string"
+    Searches duckduckgo for string
+#>
+    param([string]$Search)
+    if ($Search) {
+	Start-Process "https://duckduckgo.com/$Search"
+    }
+    else {
+	Write-Error "need search string"
+    }
+
+}
+function get-commandlocation {
+    <#
+    .Description
+    An updated which command
+    #>
+
+param([string]$Name)
+if ($Name) {
+    if ([string]::IsNullOrWhiteSpace((Get-Command $name).Source)) {
+        return (Get-Command $Name)
+    }
+    return (Get-Command $Name).Source
+}
+else { Write-Error "need name arg" }
+
+}
+set-alias which -Value get-commandlocation -Description "Standard which program"
+# TODO: Eject these functions into a module file
+
+function gitst {
+    <#
+    .Description
+    Standard git status wrapper
+    #>
+    if (get-command git -erroraction ignore) {
+        return git status
+    } else {
+        return write-error "Git not installed"
+    }
+}
+
+write-output "Loaded Begin-Module.psm1"
--- config-new/Microsoft.PowerShell_profile.ps1
+++ config-new/Microsoft.PowerShell_profile.ps1
@@ -4,9 +4,22 @@
 # Set env vars
 $env:PAGER = (get-command 'less' -erroraction ignore) ? ('less') : ('more')
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
-$newconfig = "C:\Users\Carl\OneDrive\dotfiles_backup\new-config\"
+$newconfig = "C:\Users\Carl\OneDrive\dotfiles_backup\config-new"
 $env:Path = "C:\Users\Carl\gitstuff\scripts-pwsh\bin\;$env:Path"
 $scrps = (Get-Item "~\gitstuff\scripts-pwsh")
+
+if (get-command start-condaenv -ErrorAction Ignore) {
+    start-condaenv
+} else {
+    Write-Warning "start-condaenv not on path."
+}
+
+import-module posh-git
+# Begin-Module has beginning functions for new setup
+import-module "$scrps\config-new\Begin-Module.psm1"
+$utilFuncs = "$env:OneDrive\snippets\pwsh\utilFuncs\UtilFuncs.psd1"
+import-module "$utilFuncs"
+
 
 
 $loadgitconfig = $False
@@ -20,51 +33,3 @@ if ($loadgitconfig) {
 } else {
 	write-host -fore red "`$loadgitconfig = `$False"
 }
-
-function duks {
-
-    <#
-    .Description
-    duks -Search "search string"
-    Searches duckduckgo for string
-#>
-    param([string]$Search)
-    if ($Search) {
-	Start-Process "https://duckduckgo.com/$Search"
-    }
-    else {
-	Write-Error "need search string"
-    }
-
-}
-function get-commandlocation {
-    <#
-    .Description
-    An updated which command
-    #>
-
-param([string]$Name)
-if ($Name) {
-    if ([string]::IsNullOrWhiteSpace((Get-Command $name).Source)) {
-        return (Get-Command $Name)
-    }
-    return (Get-Command $Name).Source
-}
-else { Write-Error "need name arg" }
-
-}
-set-alias which -Value get-commandlocation -Description "Standard which program"
-# TODO: Eject these functions into a module file
-
-function gitst {
-    <#
-    .Description
-    Standard git status wrapper
-    #>
-    if (get-command git -erroraction ignore) {
-        return git status
-    } else {
-        return write-error "Git not installed"
-    }
-}
-
