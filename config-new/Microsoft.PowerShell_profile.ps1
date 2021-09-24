9cada088f39f53dfa8ccd0cf26d0568ee760c1ee
Carl C <69673640+awsomesawce@users.noreply.github.com>
2021-09-23 20:57:34 -0400

    Update new pwsh profile

--- config-new/Microsoft.PowerShell_profile.ps1
+++ config-new/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,11 @@
 # Carl's New Powershell Profile
 # Date: 9/22/2021
 
-
+# Set env vars
 $env:PAGER = (get-command 'less' -erroraction ignore) ? ('less') : ('more')
+$env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
+$newconfig = "C:\Users\Carl\OneDrive\dotfiles_backup\new-config\"
+$env:Path = "C:\Users\Carl\gitstuff\scripts-pwsh\bin\;$env:Path"
 $scrps = (Get-Item "~\gitstuff\scripts-pwsh")
 
 
@@ -35,6 +38,10 @@ function duks {
 
 }
 function get-commandlocation {
+    <#
+    .Description
+    An updated which command
+    #>
 
 param([string]$Name)
 if ($Name) {
@@ -46,13 +53,18 @@ if ($Name) {
 else { Write-Error "need name arg" }
 
 }
+set-alias which -Value get-commandlocation -Description "Standard which program"
 # TODO: Eject these functions into a module file
 
 function gitst {
+    <#
+    .Description
+    Standard git status wrapper
+    #>
     if (get-command git -erroraction ignore) {
         return git status
     } else {
         return write-error "Git not installed"
     }
-}$env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
-$newconfig = "C:\Users\Carl\OneDrive\dotfiles_backup\new-config\"
+}
+
